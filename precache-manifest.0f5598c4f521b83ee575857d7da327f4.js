b0dfd0f49a706441737b9e58875c92ce940e468f
AntoineDao <antoinedao1@gmail.com>
2019-03-31 18:53:36 +0100

    Updates

---
+++ precache-manifest.0f5598c4f521b83ee575857d7da327f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dcdffd5dbaa6d487ee2",
+    "url": "/spider-react/static/css/main.5ddddeb1.chunk.css"
+  },
+  {
+    "revision": "8dcdffd5dbaa6d487ee2",
+    "url": "/spider-react/static/js/main.77a2cf98.chunk.js"
+  },
+  {
+    "revision": "7a4cbfbe5de1bd395051",
+    "url": "/spider-react/static/js/runtime~main.a2851dd9.js"
+  },
+  {
+    "revision": "d1e70a3663a4179185bc",
+    "url": "/spider-react/static/js/2.5379812d.chunk.js"
+  },
+  {
+    "revision": "af267e7c46094f00250329b2574a5e96",
+    "url": "/spider-react/index.html"
+  }
+];
